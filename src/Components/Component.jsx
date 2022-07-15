44916a4d3290534b312b15a6e298c94b1fad9c46
Srk78699 <ksirtaj29@gmail.com>
2022-07-15 18:57:37 +0530

    theme

---
+++ src/Components/Component.jsx
@@ -0,0 +1,10 @@
+import { Text, Stack } from "@chakra-ui/react";
+const Component = () => {
+  return (
+    <Stack spacing={3}>
+      <Text textStyle="subtitle1"> In love with React & Next</Text>
+      <Text textStyle="subtitle2"> In love with React & Next</Text>
+    </Stack>
+  );
+};
+export default Component;
---
+++ src/theme/textStyles.js
@@ -0,0 +1,14 @@
+export const textStyles = {
+    subtitle1: {
+      fontWeight: "400",
+      fontSize: "24px",
+      lineHeight: "34px",
+      fontFamily: "Open Sans"
+    },
+    subtitle2: {
+      fontWeight: "600",
+      fontSize: "34px",
+      lineHeight: "44px",
+      fontFamily: "Open Sans"
+    }
+  };
